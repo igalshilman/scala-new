ce7c6b88b505dc2b930801b9916c8610161eaae3
Igal Shilman <igalshilman@gmail.com>
2017-02-21 21:37:46 +0300

    adding more test examples

--- src/test/scala/MySpec.scala
+++ src/test/scala/MySpec.scala
@@ -1,13 +1,18 @@
+import org.specs2.mutable.Specification
+
 /**
   * Created by igalshilman on 20/02/2017.
   */
-class MySpec extends org.specs2.mutable.Specification {
-  "this is my specification" in {
-    "where example 1 must be true" in {
+class MySpec extends Specification {
+  "in this specification" should {
+    "example 1 must be true" in {
       1 must_== 1
     }
     "where example 2 must be true" in {
-      2 must_== 2
+      1 must_!=  2
     }
-  }
+    "and the lists should be equal" in {
+      List(1,2,3) must_== List(1,2,3)
+    }
+   }
 }
